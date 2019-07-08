891165c00bab38faa6919e46d80ec57bf16e3d95
Urmila Pradeep <urmila.pradeep@veeva.com>
2019-07-07 20:03:38 -0700

    Basic app with button

---
+++ hooks-demo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Let's learn React Hooks!
+        </p>
+        <button className="simple-button">Click Me!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
