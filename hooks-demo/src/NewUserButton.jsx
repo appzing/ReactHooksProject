dfd4ae679dd20e63af6cdbd39234315f4552bc53
Urmila Pradeep <urmila.pradeep@veeva.com>
2019-07-19 10:40:11 -0700

    Initial setup

--- hooks-demo/src/NewUserButton.jsx
+++ hooks-demo/src/NewUserButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
+//<Button variant="primary" className="new-user-button" onClick={() => props.onClick()}>Add New User</Button>
+
 export default function (props) {
     return (
-        <Button variant="primary" className="new-user-button" onClick={() => props.onClick()}>Add New User</Button>
+        <Button variant="primary" className="new-user-button">Add New User</Button>
     );
 
 }
